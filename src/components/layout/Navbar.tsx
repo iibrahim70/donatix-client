3c5225c8474de70e79ae824e1a4b9a4d6e4f60b4
iibrahim70 <iibrahiim.dev@gmail.com>
2024-03-06 14:40:30 +0600

    theme switcher

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,19 @@
+import { useTheme } from "@/providers/ThemeProvider";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { Moon, Sun } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
+
   return (
-    <nav className="shadow-md">
+    <nav className="shadow-md bg-white dark:bg-black">
       <div className="section-wrapper flex items-center justify-between py-3">
         {/* left side */}
         <h1 className="font-bold text-xl">Giver's Heaven</h1>
@@ -12,7 +22,31 @@ const Navbar = () => {
         <Link to="/">All Donations</Link>
 
         {/* right side */}
-        <Button>Login</Button>
+        <div className="flex items-center justify-between gap-3">
+          <Button>Login</Button>
+
+          {/* theme switcher */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </nav>
   );
