aa21085f52f77b0aa320aaca66db6c4d040dc9b5
iibrahim70 <iibrahiim.dev@gmail.com>
2024-03-06 14:21:48 +0600

    aded  basic navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="shadow-md">
+      <div className="section-wrapper flex items-center justify-between py-3">
+        {/* left side */}
+        <h1 className="font-bold text-xl">Giver's Heaven</h1>
+
+        {/* middle */}
+        <Link to="/">All Donations</Link>
+
+        {/* right side */}
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
