61146940f718eb59c7d53065d08b527f09f81606
iibrahim70 <aviknuhash70@gmail.com>
2024-03-16 21:36:16 +0600

    update leaderboard page

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -6,8 +6,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useTheme } from "@/providers/ThemeProvider";
+import { useSkeletonTheme } from "@/hooks/useSkeletonTheme";
 import { useGetTopDonorsQuery } from "@/redux/services/api";
+import Skeleton from "react-loading-skeleton";
 
 interface ILeaderBoard {
   donorName: string;
@@ -16,35 +17,56 @@ interface ILeaderBoard {
 }
 
 const Leaderboard = () => {
-  const { data } = useGetTopDonorsQuery(undefined);
-  console.log(data);
-  const theme = useTheme();
-  console.log(theme);
+  const { isLoading, error, data } = useGetTopDonorsQuery(undefined);
+  const { skeletonBaseColor, skeletonHighlightColor } = useSkeletonTheme();
+
+  if (error) {
+    return (
+      <div className="min-h-[calc(100dvh-64px)] flex items-center justify-center">
+        Error fetching data.
+      </div>
+    );
+  }
 
   return (
     <main className="bg-light-white dark:bg-light-black py-20 min-h-[calc(100dvh-64px)]">
       <div className="section-wrapper">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Rank</TableHead>
-              <TableHead>User Name</TableHead>
-              <TableHead>Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-
-          {data?.data?.map((item: ILeaderBoard) => (
-            <TableBody key={item?.rank}>
+        {isLoading ? (
+          Array.from({ length: 5 }).map((_, index) => (
+            <div key={index}>
+              <Skeleton
+                height={100}
+                baseColor={skeletonBaseColor}
+                highlightColor={skeletonHighlightColor}
+                className="mb-2"
+              />
+            </div>
+          ))
+        ) : (
+          <Table>
+            <TableHeader>
               <TableRow>
-                <TableCell className="font-medium">{item.rank}</TableCell>
-                <TableCell className="font-medium">{item.donorName}</TableCell>
-                <TableCell className="font-medium">
-                  ${item.totalDonation}
-                </TableCell>
+                <TableHead>Rank</TableHead>
+                <TableHead>User Name</TableHead>
+                <TableHead>Amount</TableHead>
               </TableRow>
-            </TableBody>
-          ))}
-        </Table>
+            </TableHeader>
+
+            {data?.data?.map((item: ILeaderBoard) => (
+              <TableBody key={item?.rank}>
+                <TableRow>
+                  <TableCell className="font-medium">{item.rank}</TableCell>
+                  <TableCell className="font-medium">
+                    {item.donorName}
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    ${item.totalDonation}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            ))}
+          </Table>
+        )}
       </div>
     </main>
   );
