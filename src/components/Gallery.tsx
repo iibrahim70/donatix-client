b1a5a6892fc047da1ef903cc0cf0ffccbf726a46
iibrahim70 <iibrahiim.dev@gmail.com>
2024-07-05 15:46:41 +0600

    :lipstick: added image lazy load inside hero section

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,4 @@
 import { useGetDonationsQuery } from "@/redux/services/api";
-import { IDonation } from "@/types";
 import LightGallery from "lightgallery/react";
 import lgZoom from "lightgallery/plugins/zoom";
 import "lightgallery/css/lightgallery.css";
@@ -7,6 +6,7 @@ import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
 import Skeleton from "react-loading-skeleton";
 import { useSkeletonTheme } from "@/hooks/useSkeletonTheme";
+import { ICause } from "@/types";
 
 const Gallery = () => {
   const { skeletonBaseColor, skeletonHighlightColor } = useSkeletonTheme();
@@ -16,7 +16,7 @@ const Gallery = () => {
 
   // Check if data exists before mapping
   const donationImages = data?.data?.map(
-    (donation: IDonation) => donation.donationImage
+    (donation: ICause) => donation.donationImage
   );
 
   if (error) {
@@ -29,7 +29,7 @@ const Gallery = () => {
 
   return (
     <section className="section-wrapper py-20 space-y-10">
-      <div className="text-center space-y-2">
+      <div className="text-center space-y-1.5">
         <p className="font-caveat text-flame-orange">Stories Through Images</p>
         <p>
           Explore our dynamic carousel showcasing the impact of your donations.{" "}
@@ -38,7 +38,7 @@ const Gallery = () => {
       </div>
 
       <LightGallery
-        elementClassNames="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-5 gap-10 xl:gap-5"
+        elementClassNames="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
         plugins={[lgZoom]}
       >
         {isLoading
@@ -54,7 +54,7 @@ const Gallery = () => {
           : donationImages.map((item: string, index: number) => (
               <img
                 key={index}
-                className="w-full h-full object-cover cursor-pointer rounded hover:scale-110 duration-500 lg-thumbnail"
+                className="w-full h-full object-cover cursor-pointer rounded scale-100 lg:hover:scale-110 duration-300 lg-thumbnail"
                 src={item}
                 alt={`Donation Image ${index}`}
               />
