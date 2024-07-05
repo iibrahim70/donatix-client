e5f78b5996bdbcb246ef1c8bc9aa6c41288da5fe
iibrahim70 <iibrahiim.dev@gmail.com>
2024-07-05 12:48:53 +0600

    :lipstick: added hooks for screen size and loading skeleton for cause card

--- src/pages/Causes.tsx
+++ src/pages/Causes.tsx
@@ -1,17 +1,12 @@
 import { useGetDonationsQuery } from "@/redux/services/api";
 import { ICause } from "@/types";
 import CauseCard from "@/components/cards/CauseCard";
+import CauseCardSkeleton from "@/components/skeletons/CauseCardSkeleton";
+import useScreenSize from "@/hooks/useScreenSize";
 
 const Causes = () => {
+  const { sliceCount } = useScreenSize();
   const { isLoading, error, data } = useGetDonationsQuery(undefined);
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center min-h-[calc(100dvh-64px)]">
-        loading...
-      </div>
-    );
-  }
 
   if (error) {
     return (
@@ -24,9 +19,11 @@ const Causes = () => {
   return (
     <main className="bg-light-pearl dark:bg-midnight-slate py-10">
       <div className="section-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {data?.data?.map((item: ICause) => (
-          <CauseCard data={item} />
-        ))}
+        {isLoading
+          ? Array.from({ length: sliceCount }).map((_, index) => (
+              <CauseCardSkeleton index={index} />
+            ))
+          : data?.data?.map((item: ICause) => <CauseCard data={item} />)}
       </div>
     </main>
   );
