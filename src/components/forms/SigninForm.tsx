d06d9ea8ac71fd5327517756d2ac9ec3c75a14bf
iibrahim70 <iibrahiim.dev@gmail.com>
2025-06-17 18:52:55 +0600

    :art: update signin form

--- src/components/forms/SigninForm.tsx
+++ src/components/forms/SigninForm.tsx
@@ -1,21 +1,15 @@
-import axios from "axios";
+"use client";
+
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { useLocation, useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
 import { Input } from "../ui/input";
-import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
-import { Label } from "../ui/label";
 import FormSubmit from "./FormSubmit";
+import { Eye, EyeOff } from "lucide-react";
+import { Label } from "../ui/label";
 
 const SigninFrom = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setIsLoading] = useState<boolean>(false);
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
-
 
   const {
     register,
@@ -25,75 +19,45 @@ const SigninFrom = () => {
   } = useForm();
 
   const onSubmit = async (data: FieldValues) => {
-    setIsLoading(true);
-    try {
-      const res = await axios.post(
-        "https://givers-heaven-server.vercel.app/api/v1/users/login",
-        data
-      );
-
-      if (res?.data?.success === true) {
-        navigate(from, { replace: true });
-        const userId = res?.data?.data?.user?._id;
-        localStorage.setItem("userId", userId);
-        setIsLoading(false);
-        reset();
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Signin successful!",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      } else {
-        throw new Error("Failed to signin. Please try again later.");
-      }
-    } catch (error) {
-      setIsLoading(false);
-      Swal.fire({
-        position: "top-end",
-        icon: "error",
-        title: "An error occurred",
-        text: "Something went wrong.",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    }
+    console.log(data);
   };
 
   return (
     <form className="space-y-3.5" onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-2.5">
-        <Label className="font-medium">Email</Label>
+        <Label>Email</Label>
+
         <Input
           type="email"
+          autoComplete="off"
           {...register("email", { required: true })}
         />
         {errors?.email && (
-          <span className="text-vivid-red text-sm">Enter a valid Email</span>
+          <span className="text-rose-600 text-sm">Enter a valid Email</span>
         )}
       </div>
 
       <div className="space-y-2.5">
-        <Label className="font-medium">Password</Label>
+        <Label>Password</Label>
+
         <div className="relative">
           <Input
             type={showPassword ? "text" : "password"}
             {...register("password", { required: true })}
           />
 
-          <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+          <div className="absolute inset-y-0 right-0 pr-2.5 flex items-center">
             <button
               type="button"
               onClick={() => setShowPassword((prev) => !prev)}
             >
-              {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+              {showPassword ? <Eye /> : <EyeOff />}
             </button>
           </div>
         </div>
 
         {errors?.password && (
-          <span className="text-vivid-red text-sm">Enter a valid Password</span>
+          <span className="text-rose-600 text-sm">Enter a valid Password</span>
         )}
       </div>
 
