5d9b858b08d52b03d218537966ee169a83702b44
iibrahim70 <iibrahiim.dev@gmail.com>
2025-06-23 11:45:48 +0600

    :hammer: added leaderboard page

---
+++ src/app/(root)/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+const Leaderboard = () => {
+  return (
+    <main className="bg-midnight-slate">
+      <div className="section-wrapper min-h-[calc(100dvh-56px)] flex items-center justify-center">
+        <p className="animate-bounce">Cooking...</p>
+      </div>
+    </main>
+  );
+};
+
+export default Leaderboard;
