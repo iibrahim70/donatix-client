ea97787ffb8c9c0ae72a1a3f0809f5157c6f954f
iibrahim70 <iibrahiim.dev@gmail.com>
2025-06-23 22:31:22 +0600

    :lipstick: added blog card skeleton

--- src/components/skeletons/CauseCardSkeleton.tsx
+++ src/components/skeletons/CauseCardSkeleton.tsx
@@ -4,25 +4,26 @@ export const CauseCardSkeleton = () => {
       <div className="h-40 bg-shadow-slate animate-pulse" />
 
       <div className="p-5 space-y-3.5">
-        <div className="space-y-2.5">
+        <div className="space-y-3.5">
           <div className="h-5 w-3/4 bg-shadow-slate rounded animate-pulse" />
 
           <div className="space-y-1.5">
+            <div className="h-3.5 w-full bg-shadow-slate rounded animate-pulse" />
             <div className="h-3.5 w-full bg-shadow-slate rounded animate-pulse" />
             <div className="h-3.5 w-2/3 bg-shadow-slate rounded animate-pulse" />
           </div>
         </div>
 
-        <div>
+        <div className="space-y-1.5">
           <div className="flex justify-between items-center">
             <div className="h-4 w-1/4 bg-shadow-slate rounded animate-pulse" />
             <div className="h-4 w-1/6 bg-shadow-slate rounded animate-pulse" />
           </div>
 
-          <div className="w-full bg-shadow-slate rounded-full h-2.5 mt-1.5 animate-pulse" />
+          <div className="w-full bg-shadow-slate rounded-full h-2 animate-pulse" />
         </div>
 
-        <div className="w-full mt-5 h-8 bg-shadow-slate rounded-md animate-pulse" />
+        <div className="w-full h-8 bg-shadow-slate rounded-md animate-pulse" />
       </div>
     </div>
   );
